9d7bf90c627f03e2f0ae9f2fa8dbea25086c7eed
Aditya Kokate <77008828+adiiityaaa@users.noreply.github.com>
2024-10-05 13:46:07 +0530

    Update AdditionExample.c

--- C/AdditionExample.c
+++ C/AdditionExample.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 
 int main() {
- int a,b,c;
+ int a,b,c; //define 3 variables
  printf("Enter two numbers: \n");
- scanf("%d %d", &a,&b);
- c=a+b;
+ scanf("%d %d", &a,&b); //input two numbers 3 and 4 as A and B respectively
+ c=a+b; //C=A+B=3+4=5
  printf("Sum of numbers is: %d", c);
  return 0;
 }
