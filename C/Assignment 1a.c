14ace072e1006643229f7643dbf3656a45f0cae3
Aditya Kokate <77008828+adiiityaaa@users.noreply.github.com>
2024-10-05 13:47:46 +0530

    Update and rename Area-Perimeter of Rectangle&Circle.c to Assignment 1a.c

--- C/Assignment 1a.c
+++ C/Assignment 1a.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 
 int main() {
- int l, b, r, ar, pr;
- float ac, pc;
+ int l, b, r, ar, pr; //define all variables for rectangle
+ float ac, pc; //define all variables of circle as float as answer can have decimal value
  printf("Enter length and breadth of rectangle:\n");
  scanf("%d %d", &l,&b);
  printf("Enter Radius of Circle\n");
  scanf("%d", &r);
- ac=3.14*r*r;
- ar=l*b;
- pr=2*(l+b);
- pc=2*3.14*r;
+ ac=3.14*r*r; //area of circle
+ ar=l*b; //area of rectangle
+ pr=2*(l+b); //perimeter of rectangle
+ pc=2*3.14*r; //circumference of circle
  printf("Perimeter of Rectangle: %d\nArea of Rectangle: %d\nPerimeter of Circle: %f\nArea of Circle: %f", pr, ar, pc, ac);
  return 0;
 }
