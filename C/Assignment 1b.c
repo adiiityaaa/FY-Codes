7694991a20758be7eb1a9ddb1fad527c8cacb8d0
Aditya Kokate <77008828+adiiityaaa@users.noreply.github.com>
2024-10-05 13:50:32 +0530

    Update and rename Velocity.c to Assignment 1b.c

--- C/Assignment 1b.c
+++ C/Assignment 1b.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-#include<math.h>
+#include<math.h> //We import this library to use sqrt() function in velocity formula.
 
 int main() {
  int a, b, c, d;
- float v;
+ float v; //Float is it can have decimal value
  printf("Velocity: sqrt(vo2 + 2a(x-x0))\nPlease enter the Vo, A, X and Xo:\n");
  scanf("%d %d %d %d", &a, &b, &c, &d);
- v = sqrt(a*a + 2*b*(c - d));
+ v = sqrt(a*a + 2*b*(c - d)); //Formula of Velocity in Programming Language Format
  printf("Velocity is: %f", v);
  return 0;
 }
