c373fa2e7ba4f2696a741a9717329d4b9a1eb0f7
Aditya Kokate <77008828+adiiityaaa@users.noreply.github.com>
2024-11-25 16:33:34 +0530

    Update 6b.c

--- C/Assignments/6b.c
+++ C/Assignments/6b.c
@@ -1,22 +1,34 @@
 #include <stdio.h>
 
+int occur(char x, char str[], int l) {
+ int cc = 0;
+ for(int j=0; j<l; j++) {
+  if(str[j] == x) { cc++; }
+ }
+ return cc;
+}
+
 int main() {
- int x, v, cv=0;
+ int x, vc=0, c, co;
  char str[100];
  printf("Enter the string:\n");
  fgets(str, 100, stdin);
  printf("Select the action:\n1. Number of Characters\n2. Number of Vowels\n3. Character Occurence\n");
  scanf("%d", &x);
- int y = strlen(str-1);
+ getchar();
+ int y = strlen(str)-1;
  for(int i=0; i<y; i++) {
-    if(str[i] == "a" | str[i] == "e" | str[i] == "i" | str[i] == "o" | str[i] == "u") { cv++; }
+    if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u') { vc++; }
  }
  switch(x) {
   case 1: printf("Number of characters in string: %d", y);
   break;
-  case 2: printf("You chose Tuesday.");
+  case 2: printf("Number of Vowels in string: %d", vc);
   break;
-  case 3: printf("You chose Wednesday.");
+  case 3: printf("Enter the character: \n");
+    scanf("%c", &c);
+    co = occur(c, str, y);
+    printf("Number of times %c occurs: %d", c,co);
   break;
   default: printf("You chose an incorrect action.");
   break;
